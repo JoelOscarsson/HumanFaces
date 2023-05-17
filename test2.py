0ac4dafd14e5314322f105bf934ce0580d71fee7
JoelOscarsson <j_oscarsson@hotmail.com>
2023-05-17 20:50:25 +0200

    back2beginning

---
+++ test2.py
@@ -0,0 +1,10 @@
+# installing packages
+import detectron2, torch, torchvision
+import os, distutils.core
+import sys
+sys.path.append('detectron2/detectron2/utils')
+import config
+
+from detectron2.utils.logger import setup_logger
+setup_logger()
+
